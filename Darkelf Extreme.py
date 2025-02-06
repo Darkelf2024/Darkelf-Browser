cbcc333a8cfc1a84e53e28ffa25f134ef4b7241a
Darkelf2024 <kjm489@km-consultant.pro>
2025-02-06 11:52:47 -0600

    Update Darkelf Extreme.py

--- Darkelf Extreme.py
+++ Darkelf Extreme.py
@@ -677,7 +677,7 @@ class Darkelf(QMainWindow):
     def generate_rsa_key_pair(self):
         private_key = rsa.generate_private_key(
             public_exponent=65537,
-            key_size=2048,
+            key_size=4096,
             backend=default_backend()
         )
         public_key = private_key.public_key()
