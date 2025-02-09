24875ba51b6c0d96cd196f4bbd03ecbee1b320bc
Darkelf2024 <kjm489@km-consultant.pro>
2025-02-08 23:11:28 -0600

    Update Darkelf Extreme.py
    Alignment of a personalized homepage.

--- Darkelf Extreme.py
+++ Darkelf Extreme.py
@@ -1031,7 +1031,7 @@ class Darkelf(QMainWindow):
                     justify-content: center;
                 }
                 h1 {
-                    font-size: 24px;
+                    font-size: 36px;  /* Increased font size */
                     margin-bottom: 20px;
                     color: #34C759; /* Same green as the tab */
                 }
@@ -1070,7 +1070,7 @@ class Darkelf(QMainWindow):
         </head>
         <body>
             <div class="content">
-                <h1>Welcome to Darkelf Browser</h1>
+                <h1>Darkelf Browser</h1>
                 <p>Your privacy is our priority.</p>
                 <form id="searchForm" action="https://lite.duckduckgo.com/lite/" method="get">
                     <input type="text" id="searchInput" name="q" placeholder="Search DuckDuckGo">
