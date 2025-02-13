d29bbb51ca4572374cc6bef49d81cce70f9fa5e9
Darkelf2024 <kjm489@km-consultant.pro>
2025-02-13 01:54:16 -0600

    Update Darkelf OSINT.py
    Add Nmap to tools list; a new terminal opens on click

--- Darkelf OSINT.py
+++ Darkelf OSINT.py
@@ -1246,6 +1246,7 @@ class Darkelf(QMainWindow):
             ("Sherlock", "sherlock"),
             ("Recon-ng", "recon-ng"),
             ("The Harvester", "theharvester"),
+            ("Nmap", "nmap"),
             # Google Earth alternative online tools
             ("Zoom Earth", "https://zoom.earth/"),
             ("NASA Worldview", "https://worldview.earthdata.nasa.gov/"),
@@ -1264,7 +1265,7 @@ class Darkelf(QMainWindow):
                 else:
                     raise ValueError("Invalid command format")
 
-            allowed_tools = ["sherlock", "recon-ng", "theharvester"]
+            allowed_tools = ["sherlock", "recon-ng", "theharvester", "nmap"]
             if url in allowed_tools:
                 sanitized_url = shlex.quote(url)
                 if system == "Darwin":  # macOS
