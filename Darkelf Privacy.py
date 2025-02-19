9fba63f84a2793d21cabef82099ed2e845fe7574
Darkelf2024 <kjm489@km-consultant.pro>
2025-02-19 09:34:47 -0600

    Update Darkelf Privacy.py
    Fixed exec in def main

--- Darkelf Privacy.py
+++ Darkelf Privacy.py
@@ -1594,7 +1594,7 @@ def main():
     app = QApplication(sys.argv)
     darkelf_browser = Darkelf()
     darkelf_browser.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
 if __name__ == '__main__':
     main()
